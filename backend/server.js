cfbbd87ba71fa7a6b2dc37de2d2b358c68057675
Kabilesh <kb320903@dal.ca>
2025-04-16 22:45:29 -0300

    port updated

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use("/api", sheetRoutes);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
